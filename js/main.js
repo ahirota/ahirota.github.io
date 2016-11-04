6f1e52a7bccdf248125fd238ecd3140f5f421ace
ahirota <ahirota@uci.edu>
2016-11-03 17:09:54 -0700

    First Commit
    Here's all the stuff for the website wrapped into one.

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+img = new Image();
+img.onload = function () {
+
+    canvas.height = canvas.width * (img.height / img.width);
+
+    /// step 1
+    var oc = document.createElement('canvas'),
+        octx = oc.getContext('2d');
+
+    oc.width = img.width * 0.5;
+    oc.height = img.height * 0.5;
+    octx.drawImage(img, 0, 0, oc.width, oc.height);
+
+    /// step 2
+    octx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);
+
+    ctx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5,
+    0, 0, canvas.width, canvas.height);
+}
+img.src = "img/Alex.jpg";
+
+//Source: http://stackoverflow.com/questions/19262141/resize-image-with-javascript-canvas-smoothly
